7589a5945240a56e0f00a0945acc73f35177e1b8
skyran1278 <skyran1278@gmail.com>
2020-07-05 13:07:10 +0800

    成功連上資料庫

--- server/src/server.js
+++ server/src/server.js
@@ -3,8 +3,9 @@
 /**
  * Module dependencies.
  */
-import debug from './debug.js';
+import PG from 'pg';
 
+import debug from './debug.js';
 import app from './app.js';
 
 /**
@@ -15,8 +16,19 @@ const port = process.env.PORT || '5000';
 app.set('port', port);
 
 /**
+ * Connect to database, and then
  * Create HTTP server.
  * Listen on provided port, on all network interfaces.
+ *
+ * 解決外部資料連不上 rds 問題
+ * https://docs.aws.amazon.com/vpc/latest/userguide/VPC_SecurityGroups.html#AddRemoveRules
+ * https://aws.amazon.com/tw/premiumsupport/knowledge-center/rds-cannot-connect/
  */
 
-app.listen(port, () => debug(`Listening on ${port}`));
+const client = new PG.Client();
+client.connect()
+  .then(() => app.listen(port, () => debug(`Listening on ${port}`)))
+  .catch((err) => debug('connection error', err.stack));
+// const res = await client.query('SELECT $1::text as message', ['Hello world!'])
+// console.log(res.rows[0].message) // Hello world!
+// await client.end()
